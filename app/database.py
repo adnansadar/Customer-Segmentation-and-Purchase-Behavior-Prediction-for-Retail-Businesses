f5dbf0c05c344a63b1db34c5439ab080969d300f
aftab <aftabmuddassir147@gmail.com>
2024-12-03 13:05:35 -0500

    add: connecting to AWS RDS

--- app/database.py
+++ app/database.py
@@ -1,11 +1,13 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
 
 def connect_to_db():
     conn= mysql.connector.connect(
-        host="localhost",    
-        user="root", 
-        password="root", 
-        database="retail_db" 
+       host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     cursor = conn.cursor()
     cursor.execute("SET SQL_SAFE_UPDATES = 0;")  # Disabling safe update for this session
