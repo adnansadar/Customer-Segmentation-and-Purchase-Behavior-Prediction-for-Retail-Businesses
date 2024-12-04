94e1ef6dd39a95c7f6cbb851869fc5fdc52f2e23
aftab <aftabmuddassir147@gmail.com>
2024-12-03 19:24:08 -0500

    fix: env db connection fix

--- app/database.py
+++ app/database.py
@@ -2,12 +2,14 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 
+
 def connect_to_db():
     conn= mysql.connector.connect(
-       host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=os.environ.get("DB_HOST"), 
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port =int(os.environ.get("DB_PORT"))
     )
     cursor = conn.cursor()
     cursor.execute("SET SQL_SAFE_UPDATES = 0;")  # Disabling safe update for this session
